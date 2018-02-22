b335f0c65cd8babde950f7ac2a3ad16d10e975c5
posth <marc.posth@desjardins.com>
2018-02-22 16:21:53 -0500

    Removed useless container and migrated state to main app after provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import GraphVisuelContainer from './graph-visuel/graph-visuel.container';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import GraphVisuel from './graph-visuel/components/graph-visuel.component';
+import { FormMicroservicesContainer } from './graph-visuel/containers/form-microservices.container';
 
 import './App.css';
 
-const App = () => (
-  <div>
-    <GraphVisuelContainer />
-  </div>
-)
+const mapStateToProps = state => {
+  return {
+    microservices: state.microservices,
+    config: state.config
+  }
+}
 
-export default App;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <section>
+          <GraphVisuel
+            microservices={this.props.microservices}
+            config={this.props.config}
+          />
+        </section>
+        <section>
+          <FormMicroservicesContainer />
+        </section>
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps)(App);
---
+++ src/graph-visuel/containers/form-microservices.container.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class FormMicroservicesContainer extends Component {
+
+    render() {
+        return (
+            <div>
+                <button>
+                    Fetch Microservices Data
+                </button>
+            </div>
+        )
+    }
+}
