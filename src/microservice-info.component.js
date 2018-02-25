3a21981e3ce3f4efc9815374df1ca33bc6a5b1a7
posth <marc.posth@desjardins.com>
2018-02-25 14:03:57 -0500

    Added dynamique adding of info on availability (very dirty! :( - must be better way to loop through available props passed down)

--- src/microservice-info.component.js
+++ src/microservice-info.component.js
@@ -2,17 +2,54 @@ import React from 'react';
 
 const MicroserviceInfo = ({ selectedNode }) => (
     <article>
-        <p><span>Nom du Service: </span> <span>{selectedNode.name || '[inconnu]'}</span></p>
-        <p><span>URL: </span> <span><a target="_blank" href={selectedNode.url}>{selectedNode.url || '[inconnu]'}</a></span></p>
-        <p><span>Port: </span><span>{selectedNode.port || '[inconnu]'}</span></p>
-        <p><span>ID: </span><span>{selectedNode.id || '[inconnu]'}</span></p>
-        <p><span>Type: </span><span>{selectedNode.type || '[inconnu]'}</span></p>
-        <p><span>HTTP Status: </span><span>{selectedNode.httpStatus || '[inconnu]'}</span></p>
-        <p><span>Method: </span><span>{selectedNode.method || '[inconnu]'}</span></p>
-        <p><span>Parameters: </span><span>{selectedNode.parameters || '[inconnu]'}</span></p>
-        <p><span>SoapAction: </span><span>{selectedNode.soapaction || '[inconnu]'}</span></p>
-        <p><span>Auth: </span><span>{selectedNode.auth || '[inconnu]'}</span></p>
-        <p><span>HTTP Duration: </span><span>{selectedNode.httpDuration || '[inconnu]'}</span></p>
+        {
+            Object.keys(selectedNode).length === 0 &&
+            (<h3>Cliquez sur un microservice (node) pour voir les informations</h3>
+        )}
+        {selectedNode.name && (<p>
+            <span>Nom du Service:</span>
+            <span> {selectedNode.name}</span>
+        </p>)}
+        {selectedNode.url && (<p>
+            <span>URL:</span>
+            <span><a target="_blank" href={selectedNode.url}> {selectedNode.url}</a></span>
+        </p>)}
+        {selectedNode.port && (<p>
+            <span>Port:</span>
+            <span> {selectedNode.port}</span>
+        </p>)}
+        {selectedNode.id && (<p>
+            <span>ID:</span>
+            <span> {selectedNode.id}</span>
+        </p>)}
+        {selectedNode.type && (<p>
+            <span>Type:</span>
+            <span> {selectedNode.type}</span>
+        </p>)}
+        {selectedNode.httpStatus && (<p>
+            <span>HTTP Status:</span>
+            <span> {selectedNode.httpStatus}</span>
+        </p>)}
+        {selectedNode.method && (<p>
+            <span>Method:</span>
+            <span> {selectedNode.method}</span>
+        </p>)}
+        {selectedNode.parameters && (<p>
+            <span>Parameters:</span>
+            <span> {selectedNode.parameters}</span>
+        </p>)}
+        {selectedNode.soapaction && (<p>
+            <span>SoapAction:</span>
+            <span>{selectedNode.soapaction}</span>
+        </p>)}
+        {selectedNode.auth && (<p>
+            <span>Auth:</span>
+            <span> {selectedNode.auth}</span>
+        </p>)}
+        {selectedNode.httpDuration && (<p>
+            <span>HTTP Duration:</span>
+            <span> {selectedNode.httpDuration}</span>
+        </p>)}
     </article>
 );
 
